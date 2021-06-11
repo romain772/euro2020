93c9e7e5c63a45372e60293918172b78bbf2bb93
romain772 <romain772@live.fr>
2021-06-11 10:50:37 +0200

    changes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -47,11 +47,11 @@ class UserController extends AbstractController
             $email = $postData->email;
             $password = $postData->password;
             $entityManager = $this->getDoctrine()->getManager();
-            // $user->setUsername($username);
-            // $user->setEmail($email);
-            // $user->setPassword($passwordEncoder->encodePassword($user, $password));        
-            // $entityManager->persist($user);
-            // $entityManager->flush();
+            $user->setUsername($username);
+            $user->setEmail($email);
+            $user->setPassword($passwordEncoder->encodePassword($user, $password));        
+            $entityManager->persist($user);
+            $entityManager->flush();
             return new JsonResponse(['message' => 'Votre compte a bien été créé!']);
         }
         else {
